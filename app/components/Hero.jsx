648c1c9dd6502d8dde6da3a5de959b26812bab07
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-17 20:13:54 +0100

    Hero Section

---
+++ app/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <>
+      <section className="h-screen">
+
+      </section>
+    </>
+  )
+}
+
+export default Hero;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    return (
+        <>
+            <nav className="p-7 flex items-center justify-between bg-white">
+                <div>
+                    <h1 className="text-3xl text-blue-500">Capital Cars</h1>
+                </div>
+                <ul className="flex justify-between space-x-11">
+                    <li>Home</li>
+                    <li>Inventory</li>
+                    <li>Blog</li>
+                </ul>
+                <div>
+                    <button className="px-8 py-2 rounded-xl bg-blue-500 text-white">Contact</button>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
