eb49bbe5744aac62791418a556a72342c66c02a0
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-19 21:33:45 +0100

    Search Button

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,3 +1,4 @@
+/* import '@/public/search.png' */
 const Hero = () => {
   return (
     <>
@@ -6,7 +7,11 @@ const Hero = () => {
           <h1 className="text-center text-sky-600 font-bold">Drive your dreams, Redefined</h1>
           <h2 className="text-center text-3xl text-white">Discover premium cars built for performance, comfort and style. Your perfect ride awaits expore now!</h2>
           <form action="#" className="flex space-x-4">
-            <input type="text" className="w-[600px] py-4 p-6 rounded-full" />
+            <input
+              type="text"
+              className="w-[600px] py-4 p-6 rounded-full bg-[url('/search.png')] bg-no-repeat bg-[length:20px_20px] bg-[position:1rem_50%] pl-12" // Adjusted background position
+              placeholder="Search..." // Use a space to avoid showing default placeholder text
+            />
             <button className="bg-blue-600 text-white rounded-full px-9 py-4">Search Inventory</button>
           </form>
         </div>
