bf933e248104f3487c2845bb0cf4fbc17735a0f7
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-19 20:36:40 +0100

    Features

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,8 +1,18 @@
 const Hero = () => {
   return (
     <>
-      <section className="h-screen">
-
+      <section className="h-screen p-11 bg-gray-400 flex flex-col justify-center items-center" id="hero">
+        <div className="flex flex-col items-center justify-center h-full p-56 space-y-8">
+          <h1 className="text-center text-blue-700 font-bold">Drive your dreams, Redefined</h1>
+          <h2 className="text-center text-3xl text-white">Discover premium cars built for performance, comfort and style. Your perfect ride awaits expore now!</h2>
+          <form action="#" className="flex space-x-4">
+            <input type="text" className="w-[600px] py-4 p-6 rounded-full bg-blue-300" />
+            <button className="bg-blue-600 text-white rounded-full px-9 py-4">Search Inventory</button>
+          </form>
+        </div>
+        <div className="w-full">
+          <img src="sedan.png" className="w-5 h-5" alt="" />
+        </div>
       </section>
     </>
   )
