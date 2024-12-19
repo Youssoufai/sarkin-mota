474f3662b8329205b0e4479d3646361bd787f43f
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-19 21:45:37 +0100

    About section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,12 @@
 const About = () => {
     return (
         <>
-
+            <section className="h-screen p-8">
+                <div>
+                    <h1 className="text-4xl">Our Deals</h1>
+                    <p>Experience the Freedom of Choice with Our Extensive Range of Premium Cars, Tailored to Suit Every Style and Need!</p>
+                </div>
+            </section>
         </>
     )
 }
