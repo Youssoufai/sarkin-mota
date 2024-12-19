6e343c0a477865a1cb18b3a701dae9fb3fc6fdc0
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-19 21:51:38 +0100

    About Section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,19 @@
 const About = () => {
     return (
         <>
-            <section className="h-screen p-8">
-                <div>
-                    <h1 className="text-4xl">Our Deals</h1>
+            <section className="h-screen p-8 space-y-7">
+                <div className="space-y-4">
+                    <h1 className="text-4xl">We deal with a wide range of brands</h1>
                     <p>Experience the Freedom of Choice with Our Extensive Range of Premium Cars, Tailored to Suit Every Style and Need!</p>
                 </div>
+                <section className="flex">
+                    <div>
+                        <img src="toyota.jpeg" alt="toyota" />
+                    </div>
+                    <div>
+                        <img src="bmw.jpeg" alt="bmw" />
+                    </div>
+                </section>
             </section>
         </>
     )
