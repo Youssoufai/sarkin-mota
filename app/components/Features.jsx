6d31489ab2a32af15e3a1088125be2503407c8fe
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-20 21:07:21 +0100

    Features section

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -1,29 +1,29 @@
-import { FaCar, FaDollarSign } from 'react-icons/fa';
+import { FaCar, FaDollarSign, FaHeadset, FaWallet } from 'react-icons/fa';
 const Features = () => {
     return (
         <>
-            <section className="h-screen bg-gray-200 text-black p-8">
+            <section className="h-screen bg-gray-200 text-black p-11">
                 <h1 className="text-4xl text-center">What We Do.</h1>
-                <section className="w-full p-8 flex items-center">
-                    <div className='space-y-3 flex'>
-                        <FaCar className='text-6xl text-gray-800' />
+                <section className="w-full space-x-4 p-8 flex items-center h-[80%]">
+                    <div className='space-y-3 flex flex-1 space-x-4'>
+                        <FaCar className='text-8xl text-gray-800' />
                         <div>
                             <h1 className="text-xl">Wide Range of Vehicle Options</h1>
                             <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
                         </div>
                     </div>
-                    <div className='space-y-3 flex'>
-                        <FaCar className='text-6xl text-gray-800' />
+                    <div className='space-y-3 flex space-x-4 flex-1'>
+                        <FaHeadset className='text-8xl text-gray-800' />
                         <div>
-                            <h1 className="text-xl">Wide Range of Vehicle Options</h1>
+                            <h1 className="text-xl">Exceptional Customer Service</h1>
                             <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
                         </div>
                     </div>
-                    <div className='space-y-3 flex'>
-                        <FaCar className='text-6xl text-gray-800' />
+                    <div className='space-y-3 flex flex-1 space-x-4'>
+                        <FaWallet className='text-6xl text-gray-800' />
                         <div>
-                            <h1 className="text-xl">Wide Range of Vehicle Options</h1>
-                            <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
+                            <h1 className="text-xl">Transparent Pricing and Financing Options</h1>
+                            <p>We offer fair and transparent pricing without hidden fees..</p>
                         </div>
                     </div>
                 </section>
