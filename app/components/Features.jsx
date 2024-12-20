0ad06cb5d40ba07ffb5ac012669e414b8c7c2c0e
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-20 20:51:48 +0100

    Feature

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -4,10 +4,27 @@ const Features = () => {
         <>
             <section className="h-screen bg-gray-200 text-black p-8">
                 <h1 className="text-4xl text-center">What We Do.</h1>
-                <section className="w-full p-8">
-                    <div>
+                <section className="w-full p-8 flex items-center">
+                    <div className='space-y-3 flex'>
                         <FaCar className='text-6xl text-gray-800' />
-                        <h1 className="text-xl">Wide Range of Vehicle Options</h1>
+                        <div>
+                            <h1 className="text-xl">Wide Range of Vehicle Options</h1>
+                            <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
+                        </div>
+                    </div>
+                    <div className='space-y-3 flex'>
+                        <FaCar className='text-6xl text-gray-800' />
+                        <div>
+                            <h1 className="text-xl">Wide Range of Vehicle Options</h1>
+                            <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
+                        </div>
+                    </div>
+                    <div className='space-y-3 flex'>
+                        <FaCar className='text-6xl text-gray-800' />
+                        <div>
+                            <h1 className="text-xl">Wide Range of Vehicle Options</h1>
+                            <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
+                        </div>
                     </div>
                 </section>
             </section>
