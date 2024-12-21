06bfceb2ff66e645b120ce2f303ec4ec7c2519b1
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-21 15:59:46 +0100

    Deal Section

--- app/components/Deals.jsx
+++ app/components/Deals.jsx
@@ -1,9 +1,19 @@
 const Deals = () => {
     return (
         <>
-            <section className="bg-white h-screen">
+            <section className="bg-white text-black h-screen p-11">
+                <div>
+                    <h1 className="text-4xl">Our Deals</h1>
+                </div>
+                <section className="p-8">
+                    <div>
+                        <img src="lexus.jpg" className="h-[290px] rounded-sm w-[279px]" alt="" />
+                        <div>
 
-            </section>
+                        </div>
+                    </div>
+                </section>
+            </section >
         </>
     )
 }
