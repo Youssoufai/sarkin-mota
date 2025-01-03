ea42b8cae4b911f7cb5511d282d28fb4c2a363a4
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-03 08:34:28 +0100

    Our New Designs

---
+++ app/components/Curve.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+const Curve = () => {
+    return (
+        <>
+            <section className='h-screen text-black'>
+                <h1>Curve</h1>
+            </section>
+        </>
+    )
+}
+
+export default Curve
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Box from "./components/Box";
 import Client from "./components/Client";
 import Contact from "./components/Contact";
+import Curve from "./components/Curve";
 import Deals from "./components/Deals";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
@@ -11,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Tile />
+      <Curve />
       <Box />
       <Features />
       <About />
