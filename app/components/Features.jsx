e1917d9a77a501e1633f4e8395795c4ae8c5d057
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-03 12:24:35 +0100

    Features Section: Corrections

--- app/components/Features.jsx
+++ app/components/Features.jsx
@@ -4,22 +4,22 @@ const Features = () => {
         <>
             <section className="h-full text-black p-8">
                 <h1 className="text-4xl text-center">What We Do.</h1>
-                <section className="w-full space-y-4 p-8 flex flex-col md:flex-row md:space-x-4 h-[80%]">
-                    <div className='space-y-3 flex flex-col md:flex-1 md:space-x-4'>
+                <section className="w-full space-y-4 p-8 flex flex-col md:items-center md:justify-center md:flex-row md:space-x-4 h-[80%]">
+                    <div className='text-center space-y-3 flex md:items-center flex-col md:flex-1 md:space-x-4'>
                         <FaCar className='text-8xl text-gray-800' />
                         <div>
                             <h1 className="text-xl">Wide Range of Vehicle Options</h1>
                             <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
                         </div>
                     </div>
-                    <div className='space-y-3 flex flex-col md:flex-1 md:space-x-4'>
+                    <div className='text-center space-y-3 flex md:items-center flex-col md:flex-1 md:space-x-4'>
                         <FaHeadset className='text-8xl text-gray-800' />
                         <div>
                             <h1 className="text-xl">Exceptional Customer Service</h1>
                             <p>We offer a variety of new and pre-owned vehicles, including different makes, models, and price ranges to cater to diverse customer needs.</p>
                         </div>
                     </div>
-                    <div className='space-y-3 flex flex-col md:flex-1 md:space-x-4'>
+                    <div className='text-center space-y-3 flex md:items-center flex-col md:flex-1 md:space-x-4'>
                         <FaWallet className='text-6xl text-gray-800' />
                         <div>
                             <h1 className="text-xl">Transparent Pricing and Financing Options</h1>
