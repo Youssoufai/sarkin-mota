8afbd60bfe0260a3dc83a656f42115ffdba4aeea
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-05 20:10:17 +0100

    Push

--- app/components/Mission.jsx
+++ app/components/Mission.jsx
@@ -6,7 +6,7 @@ const Mission = () => {
         <>
             <section className='h-screen bg-gray-50 p-5 md:p-11 flex flex-col md:flex-row items-center'> {/* Adjusted padding and flex direction for responsiveness */}
                 <div className='flex-1 p-5 m-4'>
-                    <img src="Customer-service.jpg" className='rounded w-full md:w-[484px] h-auto md:h-[290px]' alt="customer" /> {/* Responsive image size */}
+                    <img src="service.jpg" className='rounded w-full md:w-[484px] h-auto md:h-[290px]' alt="customer" /> {/* Responsive image size */}
                 </div>
                 <div className='flex-1 space-y-4 p-5 m-4'> {/* Added space between elements */}
                     <h1 className='text-3xl md:text-4xl'>Our Customer <span className="text-red">Service</span></h1> {/* Responsive font size */}
